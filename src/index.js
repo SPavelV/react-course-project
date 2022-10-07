72d5ef6f4cb5e30a939159e91dc6412412b700e4
Pavel Solovev <p.solovev@articul.ru>
2022-10-07 16:04:27 +0400

    Part 1. Intro, React.createElement(), JSX

--- src/index.js
+++ src/index.js
@@ -2,4 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render();
+root.render(
+  <div id='div1'>
+    <h1>Caramba!</h1>
+
+    <div id='id2'>
+      <span>Hello World</span>
+      <button type='button' onClick={() => console.log('click')}>
+        Button
+      </button>
+    </div>
+  </div>
+);
+
+// root.render(
+//   React.createElement('div', {
+//     id: 'div1',
+//     children: [
+//       React.createElement('h1', {
+//         children: 'Caramba!',
+//       }),
+//       React.createElement('div', {
+//         id: 'div2',
+//         children: [
+//           React.createElement('span', { children: 'Hello World' }),
+//           React.createElement('button', {
+//             onClick: () => console.log('click'),
+//             children: 'Button',
+//           }),
+//         ],
+//       }),
+//     ],
+//   })
+// );
