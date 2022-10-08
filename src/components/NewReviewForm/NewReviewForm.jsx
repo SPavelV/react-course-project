8c359babb2160d52989eb0b141961b8ac9fb7981
Pavel Solovev <p.solovev@articul.ru>
2022-10-08 19:00:02 +0400

    useDispatch

--- src/components/NewReviewForm/NewReviewForm.jsx
+++ src/components/NewReviewForm/NewReviewForm.jsx
@@ -1,11 +1,27 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import classnames from 'classnames';
 import styles from './styles.module.css';
 
+const FORM_ACTIONS = {
+  setName: 'setName',
+  setText: 'setText',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case FORM_ACTIONS.setName:
+      return { ...state, name: action.payload, text: '' };
+    case FORM_ACTIONS.setText:
+      return { ...state, text: action.payload };
+    default:
+      return state;
+  }
+};
+
 export const NewReviewForm = ({ className }) => {
-  const [value, setValue] = useState({ name: 'John', text: 'comment' });
-  
-  console.log('value', value);
+  const [form, dispatch] = useReducer(reducer, {});
+
+  console.log('form :>> ', form);
 
   return (
     <div className={classnames(styles.root, className)}>
@@ -13,18 +29,20 @@ export const NewReviewForm = ({ className }) => {
         <span>Name</span>
         <input
           type='text'
-          value={value.name}
-          onChange={(evt) =>
-            setValue({ ...value, name: evt.target.value, text: '' })
-          }
+          value={form.name}
+          onChange={(evt) => {
+            dispatch({ type: FORM_ACTIONS.setName, payload: evt.target.value });
+          }}
         />
       </div>
       <div className={styles.formElement}>
         <span>Text</span>
         <input
           type='text'
-          value={value.text}
-          onChange={(evt) => setValue({ ...value, text: evt.target.value })}
+          value={form.text}
+          onChange={(evt) => {
+            dispatch({ type: FORM_ACTIONS.setText, payload: evt.target.value });
+          }}
         />
       </div>
     </div>
