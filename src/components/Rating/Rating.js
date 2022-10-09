cbe9da8e3b98ec13b3ed99202c00d81a9b210240
Pavel Solovev <p.solovev@articul.ru>
2022-10-09 21:13:56 +0400

    svg

---
+++ src/components/Rating/Rating.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classnames from 'classnames';
+import styles from './styles.module.css';
+import {  } from './';
+
+export const Rating = ({ className, rating }) => {
+  return <div className={classnames(styles.root, className)}>
+    {}
+  </div>;
+};
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './styles.module.css';
 import classnames from 'classnames';
+import { ReactComponent as Star } from './img/star-gold.svg';
+// import img from './img/star-gold.svg';
 
 export const Review = ({ className, userName, text, rating }) => {
   console.log('render Review');
@@ -8,8 +10,12 @@ export const Review = ({ className, userName, text, rating }) => {
   return (
     <div className={classnames(styles.root, className)}>
       <div className={styles.user}>
-        <span className={styles.name}>User: {userName}</span>
-        <span className={styles.rating}>Rating: {rating}</span>
+        <div className={styles.name}>User: {userName}</div>
+        <div className={styles.rating}>Rating: {rating}</div>
+        {/* <div className={styles.star}></div> */}
+        {/* <img src={img} alt='star' /> */}
+        <Star className={styles.starComponent} />
+
       </div>
       <div className={styles.comment}>Comment: {text}</div>
     </div>
