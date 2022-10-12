d9ac53a023c86b6a34fb734961f5614502a5dcb2
Pavel Solovev <p.solovev@articul.ru>
2022-10-12 13:38:40 +0400

    config custormStore, root reducer

--- src/customStore/Store/Store.js
+++ src/customStore/Store/Store.js
@@ -1,15 +1,42 @@
+const restaurantReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const rootReducer = (state = {}, action = {}) => ({
+  restaurant: restaurantReducer(state.restaurants, action),
+});
+
 class Store {
   state = {};
 
   subscribers = {};
 
+  reducer() {}
+
   subscribe(key, callback) {
-    this.subscribers[key] = callback;
-    this.subscribers[key](this.state);
+    if (typeof callback === 'function') {
+      this.subscribers[key] = callback;
+      this.subscribers[key](this.state);
+    }
   }
 
   unsubscribe(key) {
     delete this.subscribers[key];
+  }
+
+  sendState() {
+    Object.keys(this.subscribers).forEach((key) => {
+      if (typeof this.subscribers[key] === 'function') {
+        this.subscribers[key](this.state);
+      }
+    });
+  }
+
+  dispatch(action) {
+    this.state = this.rootReducer(this.state, action);
   }
 }
 
@@ -18,7 +45,7 @@ export const createStore = (() => {
 
   return () => {
     if (!store) {
-      store = new Store();
+      store = new Store(rootReducer);
     }
 
     return store;
